325aaf7d51874ff0e8da53a6dc97ed5779e3b79d
Midnightboogie <liege.33.core@icloud.com>
2026-09-12 16:47:37 +0200

    fix: lightbox archivo con flechas laterales y centrado

--- src/data/archivo.ts
+++ src/data/archivo.ts
@@ -7,6 +7,7 @@ export interface PastEdition {
   description: string; // Campo listo para el futuro CMS
   artists?: string[];  
   image?: string;
+  imageBack?: string;
   link?: string; // Opcional: si no existe, la web muestra "Próximamente"
 }
 
@@ -17,7 +18,8 @@ export const pastEditions: PastEdition[] = [
     dates: "28-29 de noviembre, 2025",
     description: "Edición del festival de 2025.",
     artists: ["Stone Foundation", "BEA GB", "SYL AL", "The Lazy Tones"],
-    image: "/images/2025/2025.png"
+    image: "/images/2025/2025.jpg",
+    imageBack: "/images/2025/2025-02.jpg",
     // link eliminado temporalmente
   },
   {
@@ -26,7 +28,8 @@ export const pastEditions: PastEdition[] = [
     dates: "22-23 de noviembre, 2024",
     description: "Edición del festival de 2024.",
     artists: ["Nine Below Zero", "Los Fulanos", "Haylen"],
-    image: "/images/2024/2024.jpg"
+    image: "/images/2024/2024.jpg",
+    imageBack: "/images/2024/2024-2.jpg",
   },
   {
     year: 2023,
@@ -34,7 +37,8 @@ export const pastEditions: PastEdition[] = [
     dates: "24-25 de noviembre, 2023",
     description: "Edición del festival de 2023.",
     artists: ["Artista 1", "Artista 2", "Artista 3"],
-    image: "/images/2023/2023.jpg"
+    image: "/images/2023/2023.jpg",
+    imageBack: "/images/2023/2023-2.jpg",
   },
   {
     year: 2022,
@@ -42,7 +46,9 @@ export const pastEditions: PastEdition[] = [
     dates: "25-26 de noviembre, 2022",
     description: "Edición del festival de 2022.",
     artists: ["Artista 1", "Artista 2", "Artista 3"],
-    image: "/images/2022/2022.jpg"
+    image: "/images/2022/2022.jpg",
+    imageBack: "/images/2022/2022-2.jpg",
+
   },
   {
     year: 2021,
@@ -50,7 +56,8 @@ export const pastEditions: PastEdition[] = [
     dates: "26-27 de noviembre, 2021",
     description: "Edición del festival de 2021.",
     artists: ["Artista 1", "Artista 2", "Artista 3"],
-    image: "/images/2021/2021.jpg"
+    image: "/images/2021/2021.jpg",
+    imageBack: "/images/2021/2021-2.jpg",
   },
   {
     year: 2020,
@@ -66,7 +73,8 @@ export const pastEditions: PastEdition[] = [
     dates: "22-23 de noviembre, 2019",
     description: "Edición del festival de 2019.",
     artists: ["Artista 1", "Artista 2", "Artista 3"],
-    image: "/images/2019/2019.jpg"
+    image: "/images/2019/2019.jpg",
+    imageBack: "/images/2019/2019-1.jpg",
   },
   {
     year: 2018,
@@ -74,7 +82,9 @@ export const pastEditions: PastEdition[] = [
     dates: "23-24 de noviembre, 2018",
     description: "Edición del festival de 20218.",
     artists: ["Artista 1", "Artista 2", "Artista 3"],
-    image: "/images/2018/2018.jpg"
+    image: "/images/2018/2018.jpg",
+    imageBack: "/images/2018/2018-2.jpg",
+
   },
   {
     year: 2017,
